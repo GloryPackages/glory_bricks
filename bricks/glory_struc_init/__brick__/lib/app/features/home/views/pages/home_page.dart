a3802fe076906d4e8a5f2b8f04e8da11c207c091
Mo salah <MSalahElrayes@gmail.com>
2022-08-29 18:57:16 +0200

    fix

--- bricks/glory_struc_init/__brick__/lib/app/features/home/views/pages/home_page.dart
+++ bricks/glory_struc_init/__brick__/lib/app/features/home/views/pages/home_page.dart
@@ -1,22 +1,19 @@
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-  override
-  Widget build(BuildContext context) {
+
+  @override
+  build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Glory'),
       ),
-      body: Center(
-      
-        child:  const Text(
-              'You have pushed the button this many times:',
-            ),
-      )
-      );
-
+      body: const Center(
+        child: Text(
+          'You have pushed the button this many times:',
+        ),
+      ),
+    );
   }
-
 }
